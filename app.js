615214b17220aa9ee809e446c7ebe8d04f93b306
Andres Martin Pinto <andres.m.pinto@gmail.com>
2024-11-21 03:55:33 -0300

    estructura creada

---
+++ app.js
@@ -0,0 +1,7 @@
+let pelis= require("./peliculas");
+pelis.forEach(peli => {
+    console.log(peli)
+});
+let texto=require("./mensaje")
+console.log(texto)
+
---
+++ peliculas.js
@@ -0,0 +1,28 @@
+
+let peliculas=[
+    {
+        id:null,
+        rating:null,
+        awards:null,
+        length:null,
+        price:null,
+        genre:[
+            "Aventuras",
+            "Comedia",
+            "Suspenso"]
+        },
+        {
+            id:null,
+            rating:null,
+            awards:null,
+            length:null,
+            price:null,
+            genre:["Acción",
+                "Animación",
+                "Aventuras",
+            ]
+        }
+]
+        
+
+module.exports=peliculas; 
