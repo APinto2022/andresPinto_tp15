e74d2800f845d3fbf97e09b6168c3455b608c791
Andres Martin Pinto <andres.m.pinto@gmail.com>
2024-11-29 17:20:22 -0300

    Todo funciona ok

--- peliculas.js
+++ peliculas.js
@@ -4,9 +4,36 @@ let peliculas = [
     id: 1,
     rating: 10,
     awards: 99,
+    length: 267,
+    price: 1000,
+    genre: [ "Acción", "Animación", "Comedia", "Suspenso"],
+  }
+  {
+    titulo:"Citizen Kane",
+    id: 2,
+    rating: 9.9,
+    awards: 98,
     length: 221,
     price: 1000,
-    genre: [ "Acción", "Animación","Aventuras", "Comedia", "Suspenso"],
+    genre: [ "Acción", "Animación","Aventuras", "Comedia", ],
+  }
+  {
+    titulo:"12 Angry Men",
+    id: 3,
+    rating: 9.8,
+    awards: 97,
+    length: 121,
+    price: 1000,
+    genre: [ "Aventuras", "Comedia", "Suspenso"],
+  }
+  {
+    titulo:"12 del Patìculo",
+    id: 4,
+    rating: 7,
+    awards: 66,
+    length: 111,
+    price: 1000,
+    genre: [ "Suspenso"],
   }
 ];
 
